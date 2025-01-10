04ae32c258262a154ac0369a234a4e39da58acf4
Seo Uk Sung <92835127+GustavEiffels@users.noreply.github.com>
2025-01-10 09:43:31 +0900

    [fix] web configuer setting

--- server-java/src/main/java/kr/hhplus/be/server/common/config/web_mvc/WebMvcConfig.java
+++ server-java/src/main/java/kr/hhplus/be/server/common/config/web_mvc/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(queueTokenInterceptor)
-                .addPathPatterns("/concerts/**","/payment/**","/reservations/**","/point/**");
+                .addPathPatterns("/concerts/**","/payments/**","/reservations/**","/points/**");
         WebMvcConfigurer.super.addInterceptors(registry);
     }
 }
