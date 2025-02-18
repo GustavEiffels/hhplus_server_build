2d2420dd80daf23d0ac67e7bc1728930cb21aaf6
Seo Uk Sung <92835127+GustavEiffels@users.noreply.github.com>
2025-02-19 00:59:26 +0900

    [ test ] : Kafka Test Container 수정

--- server-java/src/test/java/kr/hhplus/be/server/KafkaContainerConfiguration.java
+++ server-java/src/test/java/kr/hhplus/be/server/KafkaContainerConfiguration.java
@@ -14,25 +14,37 @@ import java.util.Properties;
 @Configuration
 public class KafkaContainerConfiguration {
 
-    private static final KafkaContainer kafka;
+    // KafkaContainer 객체들 선언 (각각의 Kafka 브로커를 다루기 위해 3개 필요)
+    private static final KafkaContainer kafkaCluster1;
+    private static final KafkaContainer kafkaCluster2;
+    private static final KafkaContainer kafkaCluster3;
 
     static {
-        kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.1")
-                .asCompatibleSubstituteFor("apache/kafka"));
-        kafka.start();
+        // 각 Kafka 클러스터(브로커) 초기화
+        kafkaCluster1 = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+        kafkaCluster2 = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+        kafkaCluster3 = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
 
-        System.setProperty("spring.kafka.bootstrap-servers", kafka.getBootstrapServers());
+
+        // Kafka 클러스터들을 시작
+        kafkaCluster1.start();
+        kafkaCluster2.start();
+        kafkaCluster3.start();
+
+        // Kafka 브로커들에 대한 bootstrap server 설정
+        System.setProperty("spring.kafka.bootstrap-servers",
+                kafkaCluster1.getBootstrapServers() + "," + kafkaCluster2.getBootstrapServers() + "," + kafkaCluster3.getBootstrapServers());
     }
-
 
     @PostConstruct
     public void createTopic() {
         Properties properties = new Properties();
-        properties.put("bootstrap.servers", kafka.getBootstrapServers());
+        properties.put("bootstrap.servers", System.getProperty("spring.kafka.bootstrap-servers"));
         AdminClient adminClient = AdminClient.create(properties);
 
         try {
-            NewTopic newTopic = new NewTopic("my-first-topic", 1, (short) 1);  // 파티션 1개, Replication Factor 1
+            // 새로운 Kafka 토픽 생성
+            NewTopic newTopic = new NewTopic("my-first-topic", 3, (short) 1);  // 3개의 파티션, Replication Factor 3
             adminClient.createTopics(Collections.singletonList(newTopic)).all().get();
         } catch (Exception e) {
             e.printStackTrace();
@@ -43,13 +55,30 @@ public class KafkaContainerConfiguration {
 
     @PreDestroy
     public void preDestroy() {
-        if (kafka.isRunning()) {
-            kafka.stop();
+        // Kafka 클러스터 중지
+        if (kafkaCluster1.isRunning()) {
+            kafkaCluster1.stop();
+        }
+        if (kafkaCluster2.isRunning()) {
+            kafkaCluster2.stop();
+        }
+        if (kafkaCluster3.isRunning()) {
+            kafkaCluster3.stop();
         }
     }
 
     @Bean
-    public KafkaContainer kafkaContainer() {
-        return kafka;
+    public KafkaContainer kafkaCluster1() {
+        return kafkaCluster1;
+    }
+
+    @Bean
+    public KafkaContainer kafkaCluster2() {
+        return kafkaCluster2;
+    }
+
+    @Bean
+    public KafkaContainer kafkaCluster3() {
+        return kafkaCluster3;
     }
 }
