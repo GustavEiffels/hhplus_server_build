ac4b18b67e93816ad0e7b0950622d0530c9866b6
Seo Uk Sung <92835127+GustavEiffels@users.noreply.github.com>
2025-02-21 03:53:45 +0900

    [ test ] : 연결 테스트 코드 수정

--- server-java/src/test/java/kr/hhplus/be/server/kafka/ConnectionTest.java
+++ server-java/src/test/java/kr/hhplus/be/server/kafka/ConnectionTest.java
@@ -3,22 +3,17 @@ package kr.hhplus.be.server.kafka;
 import kr.hhplus.be.server.common.exceptions.BusinessException;
 import kr.hhplus.be.server.common.exceptions.ErrorCode;
 import kr.hhplus.be.server.interfaces.consumer.KafkaPlatformConsumer;
-import kr.hhplus.be.server.infrastructure.reservation.ReservationProducer;
 import kr.hhplus.be.server.domain.user.User;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -28,17 +23,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ConnectionTest {
 
     @Autowired
+    KafkaPlatformConsumer consumer;
+
+    @Autowired
     KafkaTemplate<String, String> kafkaTemplate;
 
-    private final CountDownLatch latch = new CountDownLatch(1);
-    private String receivedMessage;
-
-    @KafkaListener(topics = "my-first-topic", groupId = "my-consumer-group")
-    public void consume(ConsumerRecord<String, String> record) {
-        log.info("Received Message: {}", record.value());
-        this.receivedMessage = record.value();
-        latch.countDown();
-    }
 
     public void produce(String topic, Object message) {
         try {
@@ -60,12 +49,12 @@ public class ConnectionTest {
         // when
         produce("my-first-topic", user);
 
-        latch.await(15, TimeUnit.SECONDS);
+        consumer.awaitLatch();
 
         // then
         assertEquals(
                 new ObjectMapper().writeValueAsString(user),
-                this.receivedMessage,
+                consumer.getReceivedMessage(),
                 "Kafka Producer 로 User 를 전송하면, Consumer 로 반환 받은 값과 User 를 JSON 직렬화 한 값과 같다.");
     }
 }
