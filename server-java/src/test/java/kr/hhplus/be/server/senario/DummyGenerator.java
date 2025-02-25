ca0bdfba019bd3158f581112b9981b113211cf84
Seo Uk Sung <92835127+GustavEiffels@users.noreply.github.com>
2025-02-25 22:22:37 +0900

    [ test ] : k6 - 예약 가능한 콘서트 일정 조회 테스트

--- server-java/src/test/java/kr/hhplus/be/server/senario/DummyGenerator.java
+++ server-java/src/test/java/kr/hhplus/be/server/senario/DummyGenerator.java
@@ -1,29 +1,46 @@
 package kr.hhplus.be.server.senario;
 
+import kr.hhplus.be.server.domain.concert.Concert;
+import kr.hhplus.be.server.domain.schedule.ConcertSchedule;
 import kr.hhplus.be.server.domain.user.User;
 import kr.hhplus.be.server.domain.user.UserService;
+import kr.hhplus.be.server.infrastructure.concert.ConcertJpaRepository;
+import kr.hhplus.be.server.infrastructure.schedule.ScheduleJpaRepository;
 import kr.hhplus.be.server.infrastructure.user.UserJpaRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import java.time.LocalDateTime;
 
-import java.util.Random;
 
 @SpringBootTest
 @ActiveProfiles("local")
 public class DummyGenerator {
 
     @Autowired
-    UserJpaRepository userJpaRepository;
+    UserJpaRepository    userJpaRepository;
+
+    @Autowired
+    ConcertJpaRepository concertJpaRepository;
+
+    @Autowired
+    ScheduleJpaRepository concertScheduleRepository;
+
 
     @Autowired
     UserService userService;
 
     @Autowired
     RedisTemplate<String,Object> redisTemplate;
-    @Test
+
+
+    @DisplayName("""
+            성능 테스트를 하기 위해서 1000 명의 사용자들을 생성한다.   
+            """)
+//    @Test
     void createUser(){
         // delete all user
         deleteAllUser();
@@ -35,8 +52,44 @@ public class DummyGenerator {
         }
     }
 
+    @DisplayName("""
+            성능 테스트를 하기 위해서 1개의 콘서트와 1000개의,
+            콘서트 스케줄을 생성 한다.
+            """)
+//    @Test
+    void createConcertAndConcertSchedule(){
+        // delete concertSchedule
+        deleteAllConcertSchedule();
+        // delete concert
+        deleteAllConcert();
+
+        // Add 1 Concert
+        Concert newConcert = concertJpaRepository.save(Concert.create("ConcertTitle","ConcertPlayer"));
+
+        // Add 10000 Concert Schedule
+        for(int i = 0; i<10000; i++){
+            concertScheduleRepository.save(ConcertSchedule.builder()
+                            .concert(newConcert)
+                            .reserveStartTime(LocalDateTime.now().minusDays(1))
+                            .reserveEndTime(LocalDateTime.now().plusMonths(1))
+                            .showTime(LocalDateTime.now().plusMonths(2))
+                            .build());
+        }
+    }
+
+
+
+
+
     void deleteAllUser(){
         userJpaRepository.deleteAll();
+    }
+    void deleteAllConcert(){
+        concertJpaRepository.deleteAll();
+    }
+
+    void deleteAllConcertSchedule(){
+        concertScheduleRepository.deleteAll();
     }
 
 
