2f3a6511e288e0e1f20ae64581f9e5cd1cc3d8b3
Seo Uk Sung <92835127+GustavEiffels@users.noreply.github.com>
2025-02-26 23:30:23 +0900

    [ test ] : k6 - 토큰 발급 : 시나리오 수정 후 테스트

--- stress_test/token/CreateToken.js
+++ stress_test/token/CreateToken.js
@@ -2,33 +2,34 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-    stages: [
-      { duration: "10s", target: 200 },  
-      { duration: "10s", target: 500 },  
-      { duration: "20s", target: 1000 }, 
-      { duration: "10s", target: 500 },  
-      { duration: "10s", target: 0 },    
-    ]
-  };
-  
+  stages: [
+    { duration: "5s",  target: 1000 }, 
+    { duration: "15s", target: 1000 }, 
+    { duration: "10s", target: 700 }, 
+    { duration: "10s", target: 500 },  
+    { duration: "10s", target: 100 },  
+    { duration: "10s", target: 0 },    
+  ],
+};
 
 export default function () {
   const url = "http://localhost:8080/queue_tokens";
-
+  
+  // 1 ~ 10000 범위에서 랜덤한 userId 선택
+  const userId = Math.floor(Math.random() * (10000 - 1 + 1)) + 1;
+  
   const payload = JSON.stringify({
-    userId: 1, // Long 타입 userId 전달
+    userId: userId, 
   });
 
   const params = {
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
   };
 
   const res = http.post(url, payload, params);
 
   check(res, {
-    "is status 201": (r) => r.status === 201, 
+    "is status 201": (r) => r.status === 201,
   });
 
   sleep(1);
